012aeea20bc96778c9312d6b3dbfb251df961ab9
ur2910 <ushabarghavi.r@gmail.com>
2025-10-28 22:39:04 -0700

    Remove redirects from next.config.js
    Removed redirects for GitHub and Vercel from next.config.js.

--- next.config.js
+++ next.config.js
@@ -3,20 +3,6 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  redirects: async () => {
-    return [
-      {
-        source: "/github",
-        destination: "https://github.com/steven-tey/chathn",
-        permanent: true,
-      },
-      {
-        source: "/deploy",
-        destination: "https://vercel.com/templates/next.js/chathn",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
